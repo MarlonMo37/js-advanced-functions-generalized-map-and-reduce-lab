1e4f9c3dc2242051cbb211c2afd8329eb0183898
MarlonMoreno <marlonmoreno894@gmail.com>
2021-05-21 19:21:12 -0500

    commit tree d0750d10d0177d93e0734c44af3379053d07f534

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Add your functions here
+
+const map = function(array, callback) {
+    const r = []
+
+    for (let i = 0; i < array.length; i++) {
+        r.push(callback(array[i]))
+    }
+    return r
+}
+
+const reduce = function(array, callback, startingValue) {
+    let total = 0 
+    if (startingValue) {
+        total = startingValue
+        for (let i = 0; i < array.length; i++){
+            total = callback(array[i], total)
+        }
+        return total
+    } else {
+        total = array[0]
+        for (let i = 1; i < array.length; i++){
+            total = callback(array[i], total)
+        }
+        return total
+    }
+}
